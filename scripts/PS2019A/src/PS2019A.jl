90998517067f3f4257ca39c09bc4cb60d2b7aa88
marcelovmaciel <marcelovmaciel@protonmail.com>
2021-09-18 17:08:47 -0700

    started w0

---
+++ scripts/PS2019A/src/PS2019A.jl
@@ -0,0 +1,12 @@
+module PS2019A
+
+using DataFrames, Distributions
+using FloatingTableView
+using GLMakie
+using IJulia
+using RCall
+using StatsBase
+
+greet() = print("Hello World!")
+
+end # module
---
+++ scripts/PS2019A/src/Week0.jl
@@ -0,0 +1,106 @@
+# * Imports
+using DataFrames, DataFramesMeta, Distributions
+using FloatingTableView
+#using GLMakie
+using RCall
+using StatsBase
+using Base.Filesystem
+import FreqTables as Freq
+@rlibrary readr
+@rlibrary stringi
+
+# * County level presidential data stuff
+
+# ** Reading the data
+# taken from [[https://electionlab.mit.edu/data][Data | MIT Election Lab]]
+const dataspath = "../../../data"
+
+readdir(dataspath)
+datapath = dataspath * "/dataverse_files/countypres_2000-2020.csv"
+df = DataFrame(rcopy(read_csv(datapath)))
+
+# ** Getting New Mexico stuff
+df_nm= @subset(df, :state .== "NEW MEXICO")
+browse(df_nm)
+
+@subset(df_nm, (:year .== 2016 .| :year .== 2020))
+
+df_nm1620 = filter(x -> ((x.year == 2016) | (x.year == 2020)), df_nm)
+
+df_nm1620 |> browse
+
+# ** Droping uneeded cols
+select!(df_nm1620, Not(:mode))
+select!(df_nm1620, Not(:state))
+select!(df_nm1620, Not(:state_po))
+select!(df_nm1620, Not(:version))
+
+dropmissing!(df_nm1620, [:totalvotes])
+
+# ** Droping candidates below 5%
+df_nm1620.candidateproportions = map(x->x.candidatevotes/x.totalvotes, eachrow(df_nm1620))
+
+filtered1620df = @subset(df_nm1620, (:candidateproportions .>= 0.05))
+
+# ** County Population
+# taken from
+# https://www.census.gov/programs-surveys/popest/technical-documentation/research/evaluation-estimates/2020-evaluation-estimates/2010s-counties-total.html
+
+popdfpath = dataspath * "/co-est2020.csv"
+
+popdf = DataFrame(rcopy(read_csv(popdfpath, locale = locale(encoding = "UTF-8") )))
+
+popdf |> browse
+
+colstokeep = [ "COUNTY", "STNAME", "CTYNAME",
+               "POPESTIMATE2014", "POPESTIMATE2016",
+               "POPESTIMATE2020"]
+popdf = popdf[!,colstokeep]
+
+popdf_nm= @subset(popdf, :STNAME .== "New Mexico")
+
+popdf_nm.STNAME = map(uppercase, popdf_nm.STNAME )
+
+popdf_nm.CTYNAME = map(x->rcopy(stri_trans_toupper(x)), popdf_nm.CTYNAME)
+
+popdf_nm.CTYNAME = map(x->rstrip(replace(x, "COUNTY"=> "")), popdf_nm.CTYNAME)
+
+
+popdf_nm.CTYNAME = map(x->rstrip(replace(x, "DO\xf1A ANA"=> "DONA ANA")), popdf_nm.CTYNAME)
+
+
+
+pop16 = Dict(x.CTYNAME => x.POPESTIMATE2016 for x in eachrow(popdf_nm))
+Set(keys(pop16) |> collect)
+Set(unique(filtered1620df.county_name))
+setdiff(Set(keys(pop16) |> collect), Set(unique(filtered1620df.county_name)))
+delete!(pop16, "NEW MEXICO")
+
+filtered1620df
+
+# TODO: create a loop that will fill a vector that will be the pop col
+
+
+
+# ** Saving
+R"write.csv($df_nm, file = '../../../data/nm_counties.csv'  )"
+R"write.csv($filtered1620df, file = '../../../data/nm_counties_1620.csv'  )"
+
+
+
+# * garbarge to look later
+# # dfnm16 = filter(x -> ((x.year == 2016) ), df_nm)
+# dfnm20 = filter(x -> ((x.year == 2020) ), df_nm)
+
+#grouped_data = groupby(df_nm1620, [:year, :county_name])
+
+# totalpergroup = ((combine(grouped_data, :totalvotes => :count)) |>
+#     x -> groupby(x, [:year, :county_name]) |>
+#     x -> combine(x, :count => unique)
+#     )
+
+# combine(gdf,
+#                AsTable([:PetalLength, :SepalLength]) =>
+#                    x -> std(x.PetalLength) / std(x.SepalLength))
+
+# combine(x->x.candidatevotes/x.totalvotes, dfnm16) |> browse
