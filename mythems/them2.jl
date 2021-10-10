eba7fb61bf84d99df8bc0657e6bb37209b17cc68
marcelovmaciel <marcelovmaciel@protonmail.com>
2021-10-10 16:10:08 -0700

    them 3.4 done

---
+++ mythems/them2.jl
@@ -0,0 +1,26 @@
+import Pkg
+Pkg.activate("../scripts/PS2019A")
+using Distributions
+
+((0.99 * 0.8)
++ (0.01 * 0.1)
++ (0.01 * 0.9)
+ + (0.99 * 0.2))
+
+# * Them 3.4
+
+# 1. p = 0.5, n = 7, k = 4, thus Pr(X=k) =
+pdf(Binomial(6,0.5), 4)
+# 2. p = 0.5, n = 6, k = 4, thus Pr(X=k) =
+pdf(Binomial(6,0.5), 4)
+
+# 3. p = 0.5, n = 5, k = 3, thus Pr(X=k) =
+pdf(Binomial(5,0.5), 3)
+
+
+# 4.  p = 0.5, n = 4, k = 3, thus Pr(X=k)
+pdf(Binomial(4,0.5), 3)
+
+
+# 5. p = 0.5, n = 3, k = 3, thus Pr(X=k) =
+pdf(Binomial(3,0.5), 3)
