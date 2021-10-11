b627351ccc4cc24a45694f63bea4c76ff509551a
marcelovmaciel <marcelovmaciel@protonmail.com>
2021-10-10 17:27:31 -0700

    them 3.7 done

--- mythems/them2.jl
+++ mythems/them2.jl
@@ -9,30 +9,30 @@ using Distributions
 
 # * Them 3.4
 
-# 1. p = 0.5, n = 7, k = 4, thus Pr(X=k) =
+# 1. p = 0.5, n = 7, k = 4, thus Pr(X>=k) =
 (pdf(Binomial(7,0.5), 4) +
     pdf(Binomial(7,0.5), 5) +
     pdf(Binomial(7,0.5), 6) +
     pdf(Binomial(7,0.5), 7)
  )
 
-# 2. p = 0.5, n = 6, k = 4, thus Pr(X=k) =
+# 2. p = 0.5, n = 6, k = 4, thus Pr(X>=k) =
 (pdf(Binomial(6,0.5), 4) +
     pdf(Binomial(6,0.5), 5) +
     pdf(Binomial(6,0.5), 6)
  )
 
-# 3. p = 0.5, n = 5, k = 3, thus Pr(X=k) =
+# 3. p = 0.5, n = 5, k = 3, thus Pr(X>=k) =
 (pdf(Binomial(5,0.5), 3) +
     pdf(Binomial(5,0.5), 4) +
     pdf(Binomial(5,0.5), 5)
  )
 
 
-# 4.  p = 0.5, n = 4, k = 3, thus Pr(X=k)
+# 4.  p = 0.5, n = 4, k = 3, thus Pr(X>=k)
 (pdf(Binomial(4,0.5), 3) +
 pdf(Binomial(4,0.5), 4))
 
 
-# 5. p = 0.5, n = 3, k = 3, thus Pr(X=k) =
+# 5. p = 0.5, n = 3, k = 3, thus Pr(X>=k) =
 pdf(Binomial(3,0.5), 3)
