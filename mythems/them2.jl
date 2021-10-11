5e52de4d96f8d23a32665717b01eb18a8e8f4b7d
marcelovmaciel <marcelovmaciel@protonmail.com>
2021-10-10 20:06:56 -0700

    them 4.4 and 4.5 done

--- mythems/them2.jl
+++ mythems/them2.jl
@@ -9,30 +9,30 @@ using Distributions
 
 # * Them 3.4
 
+winningprob(n,k,p) = sum(pdf(Binomial(n,p), i) for i in k:n)
+
+
 # 1. p = 0.5, n = 7, k = 4, thus Pr(X>=k) =
-(pdf(Binomial(7,0.5), 4) +
-    pdf(Binomial(7,0.5), 5) +
-    pdf(Binomial(7,0.5), 6) +
-    pdf(Binomial(7,0.5), 7)
- )
+
+winningprob(7,4,0.5)
 
 # 2. p = 0.5, n = 6, k = 4, thus Pr(X>=k) =
-(pdf(Binomial(6,0.5), 4) +
-    pdf(Binomial(6,0.5), 5) +
-    pdf(Binomial(6,0.5), 6)
- )
-
+winningprob(6,4,0.5)
 # 3. p = 0.5, n = 5, k = 3, thus Pr(X>=k) =
-(pdf(Binomial(5,0.5), 3) +
-    pdf(Binomial(5,0.5), 4) +
-    pdf(Binomial(5,0.5), 5)
- )
-
-
+winningprob(5,3,0.5)
 # 4.  p = 0.5, n = 4, k = 3, thus Pr(X>=k)
-(pdf(Binomial(4,0.5), 3) +
-pdf(Binomial(4,0.5), 4))
-
-
+winningprob(4,3,0.5)
 # 5. p = 0.5, n = 3, k = 3, thus Pr(X>=k) =
-pdf(Binomial(3,0.5), 3)
+winningprob(3,3,0.5)
+
+
+# * Them 3.7
+
+2 * 0.6 - 1 * 0.4
+
+
+(200 * 0.6 - )
+
+35 * 0.1 - 2*0.9
+
+(35 * 100 * 0.1)
